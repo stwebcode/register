872d1eeb77513878c87d3d934c05837627c9c393
CracX <thecracx@protonmail.com>
2021-02-08 22:17:38 +0200

    Updated/added comments

--- server.php
+++ server.php
@@ -3,22 +3,42 @@ require_once 'config.php';
 
 header('Content-Type: application/json; charset=utf-8');
 
+// Abstrakta klase, kas simulē Enum objektu priekš error tipiem, kas ir attiecināmi JS definētajiem error tipiem
+abstract Class ErrorType{
 
-function out(string $message, bool $is_error=false, string $type="default"){
+    // Error tips lietotājvārdu kļūdām
+    const USERNAME = "username_error";
+
+    // Error tips paroļu kļūdām
+    const PASSWORD = "password_error";
+    
+    // Error tips nenoteiktām kļūdām
+    const NONE = "none";
+}
+
+function out(string $message, bool $is_error=false, string $type=ErrorType::NONE){
 
     /*
-                        out() - izvades funkcija
+                            out() - izvades funkcija
 
     Šo funkciju jālieto gadījumos ja serveris izvada informāciju klientam.
     Katru reizi kad kaut kas tiek izvadīts ar šo funkciju, skripts apstājas, tādā
     veidā samazinot if/else statement lietošanu.
 
     @author: CracX
+    @example: out("Invalid username", $is_error=true, ErrorType::USERNAME);
 
     */
 
     switch($is_error){
+        
+        // Ja izvade ir kļūda
         case true:
+
+            // Sūtam jebkādu error kodu lai frontend AJAX funkcija nofeilo
+            http_response_code(404);
+
+            // izveidojam ziņojumu attiecīgā formātā
             $out = array(
                 "error" => true,
                 "message" => $message,
@@ -26,7 +46,13 @@ function out(string $message, bool $is_error=false, string $type="default"){
             );
             break;
         
+        // Ja izvade ir parasts ziņojums
         case false:
+
+            // Sūtam success kodu
+            http_response_code(200);
+
+            // izveidojam ziņojumu attiecīgā formātā
             $out = array(
                 "success" => true,
                 "message" => $message
@@ -34,12 +60,16 @@ function out(string $message, bool $is_error=false, string $type="default"){
             break;
 
         default:
+            
+            // Noklusējuma rezultātā izvadām parastu ziņojumu
+            http_response_code(200);
             $out = array(
                 "success" => true,
                 "message" => $message
             );
     }
 
+    // Liekam skriptam "nomirt" un neturpināt tālāk, izvadot ziņojumu
     die(json_encode($out, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
 }
 
@@ -54,7 +84,7 @@ if($_POST['action'] == "insert_user"){
     $output = [];
 
     if(strlen($username) < 5){
-        out("Lietotājvārdam jābūt vismaz 5 simbolus garam", $is_error=true, $type="username_error");
+        out("Lietotājvārdam jābūt vismaz 5 simbolus garam", $is_error=true, $type=ErrorType::USERNAME);
     }
 
     //Ja ievadīts lietotājvārds, kurš garāks par 4 simboliem, tad pārbauda vai tāds jau eksistē DB
@@ -68,7 +98,7 @@ if($_POST['action'] == "insert_user"){
     }
 
     if(strlen($password) < 5){
-        out("Parolei jābūt vismaz 5 simbolus garai", $is_error=true, $type="password_error");
+        out("Parolei jābūt vismaz 5 simbolus garai", $is_error=true, $type=ErrorType::PASSWORD);
     }
 
     //Ja nav eroru, tad reģistrē
